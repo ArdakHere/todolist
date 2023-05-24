f51120e90598acc12909ba71c4bb89b17fcb35fb
ArdakHere <mrglok1@gmail.com>
2023-05-24 13:57:59 +0600

    First modifications with recycler view

---
+++ app/src/androidTest/java/com/example/todolist/ToDo.kt
@@ -0,0 +1,8 @@
+package com.example.todolist
+
+class ToDo (
+     val task: String,
+     val isCompleted: Boolean
+){
+
+}
---
+++ app/src/androidTest/java/com/example/todolist/ToDoAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.todolist
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ToDoAdapter(
+    var tasks: List<ToDo>) : RecyclerView.Adapter<ToDoAdapter.ToDoViewHolder>()
+ {
+    inner class ToDoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ToDoViewHolder {
+         val view = LayoutInflater.from(parent.context).inflate(R.layout.todoitem, parent)
+
+         return ToDoViewHolder(view)
+     }
+
+     override fun getItemCount(): Int {
+        return tasks.size
+     }
+
+     override fun onBindViewHolder(holder: ToDoViewHolder, position: Int) {
+        holder.itemView.apply {
+            val textView = holder.itemView.findViewById<TextView>(R.id.textView)
+            val isDone = holder.itemView.findViewById<CheckBox>(R.id.checkBox)
+            textView.text = tasks[position].task
+            isDone.isChecked = tasks[position].isCompleted
+        }
+
+     }
+ }
